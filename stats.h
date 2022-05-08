33b5d56da8a2c356efa011b863965a6265d1112e
harshi79sk <105042931+harshi79sk@users.noreply.github.com>
2022-05-08 22:14:25 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,28 @@
+#include<iostream>
 #include <vector>
+#include<math.h>
+#include<bits/stdc++.h>
+#include<numeric>
+using namespace std;
 
-namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+namespace Statistics
+{
+    
+	template <typename T=double>	//default
+	class Stats
+   	 {
+	   public:
+		T Average;
+		T Max;
+		T Min;
+		//float Average;
+		//float Max;
+		//float Min;
+		Stats();//empty array
+		//Stats(float avg,float max,float min);
+		Stats(T average,T Max,T Min);
+	};
+	template<typename T=double>
+	Stats<T> ComputeStatistics(const std::vector<T>& );
+	//Stats ComputeStatistics(const std::vector<float>&);
 }
